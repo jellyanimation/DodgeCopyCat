c9ce222e8a551c338e699abe68f8e2f01dfc8b5b
jellyanimation <gjanimation53@gmail.com>
2025-09-10 10:23:21 +0900

    2002509101022_Init
    깃허브 연동 테스트

---
+++ Assets/GitHubTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GitHubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //깃허브 용도 테스트 중입니다.
+        //연동이 잘된다면 깃허브 공간에서 해당 내용이 보일 것입니다.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
